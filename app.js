6da1860b6356dd8d16fd98eabc6cb36805d47957
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2019-08-02 11:19:12 -0400

    Check-point before continuing with implementing this sample.

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const   gameDiv    = document.getElementById('game'),
+        minNumSpan = document.getElementById('min-num'),
+        maxNumSpan = document.getElementById('max-num'),
+        guessInput = document.getElementById('guess-input'),
+        guessBtrn  = document.getElementById('guess-button'),
+        messagePara= document.getElementById('message');
+
+minNumSpan.textContent = min;
+maxNumSpan.textContent = max;
